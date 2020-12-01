c749b19004c9203c1dbc06dc7c3fecd2a5ef7dc6
racross1 <racross1@gmail.com>
2020-12-01 10:52:01 -0800

    done

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,4 +8,18 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 
-# !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+# !!! Before you run rake db:migrate, remember to fill out the other migration files -- 
+#otherwise you'll get an error resulting from the blank migration files.
+
+class CreateCostumes < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costumes do |t|
+            t.string :name 
+            t.float :price
+            t.string :image_url
+            t.string :size
+            t.timestamps
+        end 
+    end 
+end 
+
